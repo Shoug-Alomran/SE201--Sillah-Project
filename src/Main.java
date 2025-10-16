f570644edb0d18b942cf9735a2dc05efacb6aa7c
Shoug Alomran <Shoug.alomran@hotmail.com>
2025-10-16 19:19:12 +0300

    Implemented working code.

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Sillah (صلة) Preventive Health System ===");
 
+        // Create a user
+        User user = new User("Shoug");
+
+        // Add family members
+        FamilyMember f1 = new FamilyMember("Father", 45, "SCD");
+        FamilyMember f2 = new FamilyMember("Brother", 30, "Healthy");
+        user.addFamilyMember(f1);
+        user.addFamilyMember(f2);
+
+        // Check risk
+        AlertSystem alertSystem = new AlertSystem();
+        String alert = alertSystem.checkRisk(user);
+        System.out.println(alert);
+
+        // Show available clinics
+        BookingSystem bookingSystem = new BookingSystem();
+        Clinic clinic = bookingSystem.findClinic("Riyadh Heart Center");
+        if (clinic != null) {
+            System.out.println("Recommended Clinic: " + clinic.getName());
+        }
+
+        // Book appointment
+        Appointment appointment = bookingSystem.bookAppointment(user, clinic);
+        System.out.println("Appointment booked for " + appointment.getUser().getName() +
+                " at " + appointment.getClinic().getName());
+    }
+}
